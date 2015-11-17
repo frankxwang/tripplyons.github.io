867d637c5eb8bb2b5070b41daf41e2fe0fa24973
Tripp Lyons <tripplyons@gmail.com>
2015-11-17 13:43:40 -0800

    Update ttt.js

--- tttai/ttt.js
+++ tttai/ttt.js
@@ -4,11 +4,18 @@ var ctx = canvas.getContext("2d");
 function draw() {
   ctx.fillStyle = "#34495e";
   ctx.fillRect(0, 0, 300, 300);
-  ctx.fillStyle = "#ecf0f1";
-  ctx.fillRect(95, 0, 10, 300);
-  ctx.fillRect(195, 0, 10, 300);
-  ctx.fillRect(0, 95, 300, 10);
-  ctx.fillRect(0, 195, 300, 10);
+  ctx.strokeStyle = "#ecf0f1";
+  ctx.lineWidth = 20;
+  ctx.beginPath();
+  ctx.moveTo(100, 0);
+  ctx.lineTo(100, 300);
+  ctx.moveTo(200, 0);
+  ctx.lineTo(200, 300);
+  ctx.moveTo(0, 100);
+  ctx.lineTo(300, 100);
+  ctx.moveTo(0, 200);
+  ctx.lineTo(300, 200);
+  ctx.stroke();
 }
 
 draw();
