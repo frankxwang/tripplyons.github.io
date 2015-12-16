0dd67dbe7375a5be74730cec1ea8184c907fd59b
Tripp Lyons <tripplyons@gmail.com>
2015-12-16 11:50:19 -0800

    Update tttoop.js

--- tttai/tttoop.js
+++ tttai/tttoop.js
@@ -75,6 +75,7 @@ Board.prototype.setCell = function (x, y, contents) {
 };
 
 var TicTacToe = function () {
+	AI();
 	this.canvas = document.getElementById("ttt-canvas");
 	this.ctx = this.canvas.getContext("2d");
 	this.drawCellSize = 100;
