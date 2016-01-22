5543630a71f7b87c293ed34987b12ba0977d11f9
Tripp Lyons <tripplyons@gmail.com>
2016-01-22 14:30:55 -0800

    Update main.js

--- puzzle/main.js
+++ puzzle/main.js
@@ -1,17 +1,16 @@
-var canvas;
-var ctx;
-var width;
-var height;
-
 function getStyle(a,c){var b;if((b=(a.ownerDocument||document).defaultView)&&b.getComputedStyle)return c=c.replace(/([A-Z])/g,"-$1").toLowerCase(),b.getComputedStyle(a,null).getPropertyValue(c);if(a.currentStyle)return c=c.replace(/\-(\w)/g,function(a,b){return b.toUpperCase()}),b=a.currentStyle[c],/^\d+(em|pt|%|ex)?$/i.test(b)?function(b){var c=a.style.left,d=a.runtimeStyle.left;a.runtimeStyle.left=a.currentStyle.left;a.style.left=b||0;b=a.style.pixelLeft+"px";a.style.left=c;a.runtimeStyle.left=d;return b}(b):b};
 
-function init() {
+function Game(canvasId) {
 	console.log("[[INIT]]");
-	canvas = document.getElementById("canvas");
-	ctx = canvas.getContext("2d");
-	width = parseInt(getStyle(canvas, "width"));
-	height = parseInt(getStyle(canvas, "height"));
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	var width = parseInt(getStyle(canvas, "width"));
+	var height = parseInt(getStyle(canvas, "height"));
 	console.log("[[SIZE:("+width+","+height+")]]");
 	ctx.fillStyle = "#202020";
 	ctx.fillRect(0, 0, width, height);
+}
+
+function init() {
+	var game = new Game("canvas");
 }
