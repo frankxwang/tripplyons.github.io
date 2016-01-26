4c49b4b8454b0920903fc2943cb0662d4fe5e501
Tripp Lyons <tripplyons@gmail.com>
2016-01-26 13:54:37 -0800

    Update main.js

--- puzzle/main.js
+++ puzzle/main.js
@@ -52,7 +52,7 @@ function MenuState() {
 
 MenuState.prototype.draw = function(canvas) {
 	canvas.clear(palette["dark"]).color(palette["primary"]).rect(canvas.width/4, canvas.height/4, canvas.width/2, canvas.height/2);
-	canvas.color(palette["light"]).path([[4/14*canvas.width, 4/14*canvas.height], [4/14*canvas.width, 10/14*canvas.height], [10/14*canvas.width, 7/14*canvas.height]]);
+	canvas.color(palette["light"]).path([[6/18*canvas.width, 6/18*canvas.height], [6/18*canvas.width, 12/18*canvas.height], [12/18*canvas.width, 9/18*canvas.height]]);
 }
 
 function Game(canvasId) {
